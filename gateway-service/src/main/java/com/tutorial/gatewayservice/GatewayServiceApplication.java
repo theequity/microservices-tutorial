bc048b4caadf492500e5e9b5af36460e997a7c7d
theequity <josescid@hotmail.com>
2022-11-15 10:57:43 +0100

    Add files via upload

--- gateway-service/src/main/java/com/tutorial/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/tutorial/gatewayservice/GatewayServiceApplication.java
@@ -1,4 +1,4 @@
-package com.tutorial.carservice;
+package com.tutorial.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class CarServiceApplication {
+public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CarServiceApplication.class, args);
+		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
 
 }
--- user-service/src/main/java/com/tutorial/userservice/service/UserService.java
+++ user-service/src/main/java/com/tutorial/userservice/service/UserService.java
@@ -1,17 +1,40 @@
 package com.tutorial.userservice.service;
 
 import com.tutorial.userservice.entity.User;
+import com.tutorial.userservice.feignclients.BikeFeignClient;
+import com.tutorial.userservice.feignclients.CarFeignClient;
+import com.tutorial.userservice.model.Bike;
+import com.tutorial.userservice.model.Car;
 import com.tutorial.userservice.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @Service
 public class UserService {
 
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Autowired
+    CarFeignClient carFeignClient;
+
+    @Autowired
+    BikeFeignClient bikeFeignClient;
 
     public List<User> getAll() {
         return userRepository.findAll();
@@ -24,5 +47,54 @@ public class UserService {
     public User save(User user) {
         User userNew = userRepository.save(user);
         return userNew;
+    }
+
+    public List getCars(int userId) {
+        Jwt jwt = (Jwt) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Authorization", "Bearer " + jwt.getTokenValue());
+        ResponseEntity<List> cars = restTemplate.exchange("http://car-service/car/byuser/" + userId, HttpMethod.GET, new HttpEntity<>(httpHeaders), List.class);
+        return cars.getBody();
+    }
+
+    public List getBikes(int userId) {
+        Jwt jwt = (Jwt) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Authorization", "Bearer " + jwt.getTokenValue());
+        ResponseEntity<List> bikes = restTemplate.exchange("http://bike-service/bike/byuser/" + userId, HttpMethod.GET, new HttpEntity<>(httpHeaders), List.class);
+        return bikes.getBody();
+    }
+
+    public Car saveCar(int userId, Car car) {
+        car.setUserId(userId);
+        Car carNew = carFeignClient.save(car);
+        return carNew;
+    }
+
+    public Bike saveBike(int userId, Bike bike) {
+        bike.setUserId(userId);
+        Bike bikeNew = bikeFeignClient.save(bike);
+        return bikeNew;
+    }
+
+    public Map<String, Object> getUserAndVehicles(int userId) {
+        Map<String, Object> result = new HashMap<>();
+        User user = userRepository.findById(userId).orElse(null);
+        if(user == null) {
+            result.put("Mensaje", "no existe el usuario");
+            return result;
+        }
+        result.put("User", user);
+        List<Car> cars = carFeignClient.getCars(userId);
+        if(cars.isEmpty())
+            result.put("Cars", "ese user no tiene coches");
+        else
+            result.put("Cars", cars);
+        List<Bike> bikes = bikeFeignClient.getBikes(userId);
+        if(bikes.isEmpty())
+            result.put("Bikes", "ese user no tiene motos");
+        else
+            result.put("Bikes", bikes);
+        return result;
     }
 }
